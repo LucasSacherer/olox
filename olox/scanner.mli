b802109a08111e302b981eea70fa7dee11c5db7f
Lucas Sacherer <lucas.sacherer@gmail.com>
2020-05-25 18:53:24 -0400

    Documented scanner and token interfaces.

--- olox/scanner.mli
+++ olox/scanner.mli
@@ -1,2 +1,9 @@
+(** This module provides the olox implementation of the lox scanner. For the moment, it only
+    provides a simple string->token interface. The token types are stored in the {!Olox.Token}
+    module. *)
+
 val scan_tokens :
   string -> (Token.token list, Reporting.error_record list) result
+(** Given a string, scans all tokens. Assumes that the end of the string is the end of the last
+    token, so always make sure to feed full tokens. Read {!Olox.Token} for more information on
+    what tokens look like. *)
--- olox/token.mli
+++ olox/token.mli
@@ -1,3 +1,8 @@
+(** This module contains the token type created by the olox scanner and consumed by the parser. *)
+
+(** This type contains all the support token types. Note that the [String] type contains the value
+    of the string literal without the surrounding quotes. The [Number] type contains the
+    float representation of the number literal. *)
 type token_type =
   (* single char tokens *)
   | LeftParen
@@ -45,14 +50,29 @@ type token_type =
   | Comment
   | EOF
 
+(** The token type produced by the scanner and consumed by the parser. [lexeme] 
+    contains the exact string used to create the token (so, lexems of [String] tokens
+    include the quotes on both ends). [line] contains the line number where the lexem
+    was found. For a multi line token, the first line is used.*)
 type token = {token_type: token_type; lexeme: string; line: int}
 
 val get_token_name : token_type -> string
+(** Given a [token_type], returns the matching name. The name for a [token_type] is
+    exactly the same as the name of the type (so, [Fun] returns ["Fun"]).*)
 
 val print_token : token -> unit
+(** Given a [token], prints it's string representation to stdout. For more information
+    on the string representation, see {!Token.string_of_token}. *)
 
 val print_token_list : token list -> unit
+(** Just like {!Token.print_token}, but takes a list of [token]. Different tokens are
+    seperated by newlines. *)
 
 val string_of_token : token -> string
+(** Generates a string representation of a [token]. The format is [token_type<lexem:line>]
+    where [token_type], [lexem], and [line] are replaced by the matching values in the
+    [token] struct. *)
 
 val string_of_token_list : token list -> string
+(** Just like {!Token.string_of_token}, but takes a list of [token]. Different tokens
+    are seperated by a space. *)
